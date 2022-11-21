d3e6c0ab7bc06c3792f4f3f7de0666326097e637
kausko <kaustubhodak1@gmail.com>
2022-11-21 18:14:23 -0500

    test: component selection dropdown

---
+++ src/main/java/Demo/DemoComponents.java
@@ -0,0 +1,42 @@
+package Demo;
+
+import javax.swing.*;
+
+public class DemoComponents {
+
+    public static JTextField textField = new JTextField("");
+    public static JCheckBox checkBox = new JCheckBox("");
+    public static JRadioButton radioButton = new JRadioButton("");
+    public static JMenuItem menuItem = new JMenuItem("");
+    public static JPasswordField passwordField = new JPasswordField("");
+    public static JToggleButton toggleButton = new JToggleButton("");
+    public static JTextArea textArea = new JTextArea("");
+    public static JLabel label = new JLabel("");
+    public static JButton button = new JButton("");
+
+
+//    public static JPopupMenu popupMenu = new JPopupMenu("");
+//    public static JInternalFrame internalFrame = new JInternalFrame("");
+//    public static JFormattedTextField formattedTextField = new JFormattedTextField("");
+//    public static JToolBar toolBar = new JToolBar("");
+//    public static JMenu menu = new JMenu("");
+//    public static JComboBox<String> comboBox = new JComboBox<>();
+//    public static JSlider slider = new JSlider();
+//    public static JSpinner spinner = new JSpinner();
+//    public static JProgressBar progressBar = new JProgressBar();
+//    public static JList<String> list = new JList<>();
+//    public static JTable table = new JTable();
+//    public static JTree tree = new JTree();
+//    public static JTabbedPane tabbedPane = new JTabbedPane();
+//    public static JMenuBar menuBar = new JMenuBar();
+//    public static JDesktopPane desktopPane = new JDesktopPane();
+//    public static JColorChooser colorChooser = new JColorChooser();
+//    public static JFileChooser fileChooser = new JFileChooser();
+//    public static JEditorPane editorPane = new JEditorPane();
+//    public static JSeparator separator = new JSeparator();
+//    public static JTextPane textPane = new JTextPane();
+//    public static JPanel panel = new JPanel();
+//    public static JScrollPane scrollPane = new JScrollPane();
+//    public static JSplitPane splitPane = new JSplitPane();
+
+}
---
+++ src/main/java/Demo/Main.java
@@ -0,0 +1,155 @@
+package Demo;
+
+import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+public class Main {
+
+    private final JComboBox<Field> comboBox;
+    private final JButton addButton;
+    private final JButton applyButton;
+    private final JTextArea textArea;
+    private final JPanel displayPanel;
+
+    private final JTable componentsList;
+    private final DefaultTableModel componentsListModel;
+
+    public Main() throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+        comboBox = new JComboBox<>();
+        addButton = new JButton("Add");
+        applyButton = new JButton("Apply constraints");
+        textArea = new JTextArea();
+        displayPanel = new JPanel();
+        componentsListModel = new DefaultTableModel();
+        componentsListModel.addColumn("Components");
+        componentsList = new JTable(componentsListModel);
+        setupListeners();
+    }
+
+    private void setupListeners() {
+
+        comboBox.setRenderer(new DefaultListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                setText(((Field) value).getType().getSimpleName());
+                return this;
+            }
+        });
+
+        componentsList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                var components =  displayPanel.getComponents();
+                for (Component c : components) {
+                    var co = (JComponent) c;
+                    co.setBorder(null);
+                }
+                int row = componentsList.getSelectedRow();
+                if (row == -1) return;
+                var comp = (JComponent) displayPanel.getComponent(row);
+                comp.setBorder(BorderFactory.createLineBorder(Color.RED));
+            }
+        });
+
+        componentsListModel.addTableModelListener(e -> {
+
+            try {
+                int row = e.getFirstRow();
+                int column = e.getColumn();
+                if (e.getType() != TableModelEvent.UPDATE)
+                    return;
+                var comp = (JComponent) displayPanel.getComponent(row);
+
+                comp
+                        .getClass()
+                        .getMethod("setText", String.class)
+                        .invoke(comp, (String) componentsListModel.getValueAt(row, column));
+
+                comp.setBorder(null);
+                displayPanel.revalidate();
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        });
+
+        addButton.addActionListener(btnEvent -> {
+            Field selectedItem = (Field) comboBox.getSelectedItem();
+            try {
+                assert selectedItem != null;
+                Class<?> type = selectedItem.getType();
+                Object comp = type
+                        .getConstructor(String.class)
+                        .newInstance(type.getSimpleName());
+
+                DefaultTableModel model = (DefaultTableModel) componentsList.getModel();
+                model.addRow(new Object[]{type.getSimpleName()});
+                displayPanel.add((Component) comp);
+                displayPanel.revalidate();
+
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+    }
+
+    public void run() {
+
+        Arrays.stream(DemoComponents.class.getFields()).forEach(comboBox::addItem);
+
+        var addComboPanel = new JPanel();
+        addComboPanel.setBorder(BorderFactory.createTitledBorder("Select Component"));
+        addComboPanel.add(comboBox);
+        addComboPanel.add(addButton);
+
+        var componentsScrollPane = new JScrollPane(componentsList);
+        componentsScrollPane.setBorder(BorderFactory.createTitledBorder("Edit Component text"));
+
+        var textAreaScrollPane = new JScrollPane(textArea);
+        textAreaScrollPane.setBackground(Color.WHITE);
+        textAreaScrollPane.setBorder(BorderFactory.createTitledBorder("Add Constraints"));
+
+        var horizontalSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, componentsScrollPane, textAreaScrollPane);
+        horizontalSplitPane.setResizeWeight(0.5);
+
+        var controlPanel = new JPanel(new GridBagLayout());
+        var gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        controlPanel.add(addComboPanel, gbc);
+        gbc.weighty = 1;
+        gbc.gridy = 1;
+        controlPanel.add(horizontalSplitPane, gbc);
+        gbc.weighty = 0;
+        gbc.gridy = 2;
+        controlPanel.add(applyButton, gbc);
+
+
+        var verticalSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, controlPanel, displayPanel);
+        verticalSplitPane.setResizeWeight(0.15);
+
+        var frame = new JFrame("JAutoLayout Demo");
+
+        frame.add(verticalSplitPane);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Main main = new Main();
+        SwingUtilities.invokeLater(main::run);
+    }
+}
